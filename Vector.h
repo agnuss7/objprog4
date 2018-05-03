90788c971282da1f3d619e9fc49660035a0d706f
agne burokaite <agbu4307@kiss.MIF>
2018-05-03 12:31:22 +0300

    V0.1 pradedu

---
+++ Vector.h
@@ -0,0 +1,12 @@
+class Vector {
+private:
+std::size_t sz;
+double *thing;
+
+
+
+
+
+
+
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Vector.h"
+#include "metodai.cpp"
+using std::cout;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
