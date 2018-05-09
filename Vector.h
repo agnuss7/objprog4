943f24e5c42eaef892bb25bf64b5f2e05a548ee4
agne burokaite <agne.burokaite@gmail.com>
2018-05-10 00:25:41 +0300

    Pradedu.

--- Vector.h
+++ Vector.h
@@ -1,12 +1,21 @@
+#pragma once
+template <class V>
 class Vector {
 private:
 std::size_t sz;
-double *thing;
-
-
-
-
-
-
-
+std::size_t cp;
+V *el;
+public:
+Vector(): sz(0), cp(0),el(new V[sz]){};
+void reserve(std::size_t b);
+void resize(std::size_t b);
+void pop_back();
+void push_back(V a);
+void print();
+std::size_t size() const;
+std::size_t capacity() const;
+double &operator[](std::size_t b){
+return el[b];
 }
+
+};
--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <chrono>
 #include "Vector.h"
 #include "metodai.cpp"
 using std::cout;
-
+const std::size_t N=100000;
+typedef std::chrono::high_resolution_clock Clock;
+typedef std::chrono::milliseconds TimeType;
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::chrono::high_resolution_clock::time_point t1;
+    std::chrono::high_resolution_clock::time_point t2;
+    Vector<double> A2;
+    t1=Clock::now();
+    A2.reserve(N);
+    for(double i=0.1;i<N;i++)
+    {
+        A2.push_back(i);
+    }
+    t2=Clock::now();
+    cout<<(std::chrono::duration_cast<TimeType>(t2 - t1)).count()<<" milisekundes \n";
+    std::vector<double> A1;
+    t1=Clock::now();
+    for(double i=0.1;i<N;i++)
+    {
+        A1.push_back(i);
+    }
+    t2=Clock::now();
+    cout<<(std::chrono::duration_cast<TimeType>(t2 - t1)).count()<<" milisekundes \n\n";
+
     return 0;
 }
